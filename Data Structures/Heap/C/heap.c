79393307f13bb994338adfef489f7fc8e2a52e60
Travis Avey <avey.travis@gmail.com>
2018-01-24 10:52:04 -0600

    Finishing up on Heap in C
    Got bugs fixed (book has bugs obviously)
    
    May add a couple of the algorithms for the (buggy) book

--- Data Structures/Heap/C/heap.c
+++ Data Structures/Heap/C/heap.c
@@ -2,24 +2,29 @@
 #include <stdlib.h>
 #include "heap.h"
 
+/*
+    This method handles initializing the new heap
+*/
 struct heap *initHeap(int capacity, int type) {
+    // allocate memory for the heap
     struct heap *h = (struct heap *) malloc(sizeof(struct heap));
-
+    // if null, output message and exit method
     if (h == NULL) {
         fprintf(stderr, "There was a problem initializing the heap\n");
         return NULL;
     }
-
+    // init the Heap ADT
     h->type = type;
     h->count = 0;
     h->capacity = capacity;
-
+    // allocate memory for array
     h->data = (int *) malloc(sizeof(int) * h->capacity);
-
+    // if null, output message and exit
     if (h->data == NULL) {
         fprintf(stderr, "There was a problem initializing the heap array\n");
         return NULL;
     }
+    // return the newly created heap
     return h;
 }
 
@@ -34,18 +39,32 @@ int parent(struct heap *h, int i) {
     return (i-1) / 2;
 }
 
+/*
+    This method returns the location
+    of the left child (from parent i)
+*/
 int leftChild(struct heap *h, int i) {
+    // left is as follows:
     int left = 2 * i + 1;
-
+    // if left is greater than count
     if (left >= h->count)
+        // it's not right, return -1
         return -1;
+    // return position
     return left;
 }
 
+/*
+    This method returns the location
+    of the right child (of parent i)
+*/
 int rightChild(struct heap *h, int i) {
+    // the right is as follows:
     int right = 2 * i + 2;
+    // if greater, error return -1
     if (right >= h->count)
         return -1;
+    // return right
     return right;
 }
 
@@ -71,63 +90,107 @@ int getMax(struct heap *h) {
 */
 void percolateDown(struct heap *h, int i) {
     int left, right, max, temp;
+    // get the left and right child
     left = leftChild(h, i);
     right = rightChild(h, i);
-
+    // check left
     if (left != -1 && h->data[left] > h->data[i])
+        // set left as max
         max = left;
     else
+        // else max is i
         max = i;
+    // check right
     if (right != -1 && h->data[right] > h->data[max])
+        // set max as right
         max = right;
     
+    // if max isn't i
     if (max != i) {
+        // swap data[i] and data[max]
         temp = h->data[i];
         h->data[i] = h->data[max];
         h->data[max] = temp;
-    }
 
-    percolateDown(h, max);
+        // bug here, should only be called
+        // if max is not equal to i
+        // was causing segmentaion fault
+        percolateDown(h, max);
+    }
 }
 
+/*
+    This method delete's the max (root)
+    from the heap and returns it
+*/
 int deleteMax(struct heap *h) {
+    // if the count is 0, nothing to delete
     if (h->count == 0)
+        // return -1
         return -1;
+    // get the data from the root
     int data;
     data = h->data[0];
-
+    // set the root to the most bottom child
     h->data[0] = h->data[h->count-1];
+    // decrement count
     h->count--;
+    // percolate down to re-heapify the heap
     percolateDown(h, 0);
+    // return the deleted max (root)
     return data;
 }
 
+/*
+    This method handles resizing the
+    heap by 2 if while inserting we
+    reach the max
+*/
 void resizeHeap(struct heap *h) {
+    // create copy of data array
     int *old = h->data;
+    // reallocate enough memory space for X2 the capacity
     h->data = (int *) malloc(sizeof(int) * h->capacity * 2);
-
+    // if null, memory error
     if (h->data == NULL) {
+        // output message and exit method
         fprintf(stderr, "There was a problem reallocating memory in resizing the heap\n");
         return;
     }
-
+    // loop over and copy into new reallocated array
     for (int i=0; i<h->capacity; i++)
         h->data[i] = old[i];
+    // set capacity
     h->capacity *= 2;
     
-    free(old);
 }
 
-
+/*
+    This method handles inserting
+    a new node into the heap
+*/
 void insert(struct heap *h, int data) {
+    // if we are at capacity, resize
     if (h->count == h->capacity)
         resizeHeap(h);
+    
     int i;
+    // increment the count
     h->count++;
+    // get the count
     i = h->count-1;
-    while (i >= 0 && data > h->data[(i-1)/2]) {
+    // keep looping while i is less than 0
+    // and the data is greater than the parent node
+    // in essence, the percolates the data up into 
+    // the correct position of the heap
+    while (i > 0 && data > h->data[(i-1)/2]) {
+        // set the data location to parent
         h->data[i] = h->data[(i-1)/2];
+        // move to parent
         i = (i-1) / 2;
     }
+    // when fall out of while loop
+    // we are at the right place 
+    // to insert the data
     h->data[i] = data;
 }
--- Data Structures/Heap/C/main.c
+++ Data Structures/Heap/C/main.c
@@ -10,6 +10,19 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     } else {
         printf("All systems OK\nContinue with the Heap\n");
+        printf("Inserting 8\n");
+        insert(H, 8);
+        printf("Inserting 10\n");
+        insert(H, 10);
+        insert(H, 24);
+        insert(H, 2);
+        insert(H, 38);
+        printf("getMax: %i\n", getMax(H));
+        printf("deleteMax: %i\n", deleteMax(H));
+        printf("deleteMax: %i\n", deleteMax(H));
+        printf("deleteMax: %i\n", deleteMax(H));
+        printf("deleteMax: %i\n", deleteMax(H));
+        printf("getMax: %i\n", getMax(H));
     }
 
     return EXIT_SUCCESS;
