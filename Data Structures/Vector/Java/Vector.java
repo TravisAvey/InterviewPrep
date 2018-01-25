30f9f74555f50efb69302adfce2e2768eef4fc41
Travis Avey <avey.travis@gmail.com>
2018-01-25 09:53:45 -0600

    Changed vector to be a generic class
    Added comments
    
    TODO: check if that while loop ... just realized that this method will remove all of the item in the vector.  Possibly add a new method called removeAll(T item) that will remove all and change remove(T Item) to only removing the first found?

--- Data Structures/Vector/Java/Vector.java
+++ Data Structures/Vector/Java/Vector.java
@@ -1,60 +1,74 @@
-// TODO: look at making this a generic class
-public class Vector
-{
+public class Vector<T> {
+    
+    private static final int SIZE = 16;
     private int size;
     private int length;
-    private int[] array;
-
-    // Constructor, with size
-    public Vector(int size)
-    {
-        this.size = size;
-        this.array = new int[this.size];
-        this.length = 0;
-
-    }
+    private T[] array;
 
     // default constructor
-    public Vector()
-    {
-        this.array = new int[16];
+    public Vector() {
+        this.array = (T[]) new Object[SIZE];
         this.length = 0;
     }
 
+
+    // Constructor, with size
+    public Vector(int size) {
+        // only if size is greater than 0
+        if (size > 0) {
+            // set the size, init array and length
+            this.size = size;
+            this.array = (T[]) new Object[this.size];
+            this.length = 0;
+        } else {
+            // error! can't be less than 1
+            System.out.println("Error: size cannot be less than 1");
+            throw new ArrayIndexOutOfBoundsException();
+        }
+
+    }
+
+
     // push an item onto the vector
-    public void push(int item)
-    {
-        if (this.length == this.size)
-        {
+    public void push(T item) {
+        // if we are full, resize array
+        if (this.length == this.size) {
             resize(2);
         }
+        // set item at current position
         this.array[length] = item;
+        // increment to next
         this.length++;
 
     }
 
     // removes last item from vector and returns it
-    public int pop()
-    {
-        int item = array[this.length-1];
-        if (this.length > 0)
-        {
+    public T pop() {
+        // get the last item in vector
+        T item = array[this.length-1];
+        // as long as length is greater than 0
+        if (this.length > 0) {
+            // decrement length
             this.length--;
-
+            // if len is less than 1/4 size
             if (this.length < this.size / 4)
+                // resize the array
                 resize(0.5);
-
+            // return item
             return item;
         }
         else
-            return -1;
+            // return null, b/c no items
+            return null;
     }
 
-    public int at(int index)
-    {
+    // This method returns the object at the index
+    public T at(int index) {
+        // if out of range, throw exception
         if (index >= this.length)
             throw new IndexOutOfBoundsException();
         else
+            // return the object at the index
             return this.array[index];
     }
 
@@ -72,25 +86,24 @@ public class Vector
      * @param item, the element to find in the vector
      * @return the index of the item, -1 if not found
      */
-    public int find(int item)
-    {
-        for (int i=0; i<this.length; i++)
-        {
+    public int find(T item) {
+        // loop over the array
+        for (int i=0; i<this.length; i++) {
+            // if item is current
             if (this.array[i] == item)
+                // return the index
                 return i;
         }
-
+        // fall off array? return -1 for not found
         return -1;
     }
 
     // insert item at the index supplied
-    public void insert(int item, int index)
-    {
+    public void insert(T item, int index) {
         // if the index is out of range, throw error
         if (index >= this.length)
             throw new IndexOutOfBoundsException();
-        else
-        {
+        else {
             // if the current length is the size of array
             // resize the array by a factor of 2
             if (this.length == this.size)
@@ -100,7 +113,7 @@ public class Vector
             this.length++;
 
             // everything from index to the right copy
-            int[] temp = new int[this.size];
+            T[] temp = (T[]) new Object[this.size];
 
             // init a pointer j, to start at the index supplied
             int j = index;
@@ -121,47 +134,50 @@ public class Vector
     }
 
     // prepends the vector at index 0 with item supplied
-    public void prepend(int item)
-    {
+    public void prepend(T item) {
         insert(item, 0);
     }
 
     // deletes item at index supplied
-    public void delete(int index)
-    {
+    public void delete(int index) {
         // from the index supplied, shift vector left
         for (int i=index; i<this.length-1; i++)
             this.array[i] = this.array[i+1];
         // decrement the length of the vector
         this.length--;
     }
-
-    public void remove(int item)
-    {
-        while (true)
-        {
+    
+    // removes the object from the vector
+    public void remove(T item) {
+        
+        // while true ??
+        // TODO: is this while loop necessary??
+        while (true) {
+            // find the item
             int i = find(item);
+            // if item is not found, break;
             if (i == -1)
                 break;
+            // delete the item
             delete(i);
         }
     }
 
     // print out the vector
-    public void print()
-    {
+    public void print() {
         for (int i=0;i<this.length;i++)
             System.out.println(this.array[i]);
     }
 
     // resize the vector by the factor supplied
-    // TODO: make sure resizing by 1/2 works!
-    private void resize(double factor)
-    {
+    private void resize(double factor) {
+        // multiply the size by factor
         this.size *= factor;
-
-        int[] temp = this.array.clone();
-        this.array = new int[size];
+        // create a temp array copying over the current array
+        T[] temp = this.array.clone();
+        // set array to a new array with new size
+        this.array = (T[]) new Object[size];
+        // copy the temp array into the this.array with it's new size
         System.arraycopy(temp, 0, this.array, 0, temp.length);
     }
 
