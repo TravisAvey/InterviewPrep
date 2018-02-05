03d8a62b1cd45ed342d1647480aaff007a8d68de
Travis Avey <avey.travis@gmail.com>
2018-02-05 10:56:04 -0600

    Added comments and minor fixes
    Changed all ints (non operator methods) to use u_int64_t

--- Data Structures/Vector/C++/Vector.h
+++ Data Structures/Vector/C++/Vector.h
@@ -10,7 +10,7 @@
 template <class T>
 class Vector {
 private:
-    const int SIZE = 16;
+    const u_int64_t SIZE = 16;
     int mSize;
     T *data;
     u_int64_t mLength;
@@ -19,7 +19,7 @@ private:
 public:
 
     Vector();
-    explicit Vector(int);
+    explicit Vector(u_int64_t);
 
     void push_back(T);
     void pop_back();
@@ -35,6 +35,9 @@ public:
     friend std::ostream &operator<<(std::ostream, const Vector<T>&);
 };
 
+/*
+    Default constructor
+*/
 template <class T>
 Vector<T>::Vector() {
     data = new T[SIZE];
@@ -42,8 +45,11 @@ Vector<T>::Vector() {
     mSize = SIZE;
 }
 
+/*
+    Constructor with size to init with
+*/
 template <class T>
-Vector<T>::Vector(int size) {
+Vector<T>::Vector(u_int64_t size) {
     if (size > 0) {
         data = new T[size];
         mLength = 0;
@@ -53,59 +59,112 @@ Vector<T>::Vector(int size) {
     }
 }
 
+/*
+    This method adds one item to
+    the vector
+*/
 template <class T>
 void Vector<T>::push_back(T item) {
+    // if the size is greater than array
     if (mLength >= mSize -1)
+        // resize the array
         resize();
-
+    // add item to the array
     data[mLength++] = item;
 }
 
+/*
+    This method removes the last
+    item from the vectir
+*/
 template <class T>
 void Vector<T>::pop_back() {
+    // set last item to int min
+    // and reduce the length by one
     data[mLength--] = INT64_MIN;
 }
 
+/*
+    This method returns the front item
+    from the vector
+*/
 template <class T>
 T Vector<T>::front() const {
     return data[0];
 }
 
+/*
+    This method returs the back item
+    from the vector
+*/
 template <class T>
 T Vector<T>::back() const {
     return data[mLength-1];
 }
 
+/*
+    This method returns the item at
+    the index supplied by the user
+*/
 template <class T>
 T Vector<T>::at(u_int64_t index) const {
     return data[index];
 }
 
+/*
+    This method returns true if
+    the vector is emtpy, false otherwise
+*/
 template <class T>
 bool Vector<T>::empty() const {
     return mLength == 0;
 }
 
+/*
+    This method clears the array
+    and 'resets'
+*/
 template <class T>
 void Vector<T>::clear() {
+    // set all items to int min
+    for (int i=0; i<mLength; i++)
+        data[i] = INT64_MIN;
+    // set length and size
     mLength = 0;
-    mSize = SIZE;
+    // set array to new array with default size
+    // or size user set in constructor
     data = new T[mSize];
 }
 
+/*
+    This method returns how many elements are
+    in the vector
+*/
 template <class T>
 u_int64_t Vector<T>::size() const {
     return mLength;
 }
 
+/*
+    This is a helper method that doubles
+    the array size
+*/
 template <class T>
 void Vector<T>::resize() {
+    // create a new array
     auto *newArr = new T[mSize*2];
+    // copy old data into new array
     std::copy(data, data + std::min(mSize, mSize*2), newArr);
+    // set the data's address to newArr address
     data = newArr;
+    // double the size
     mSize *= 2;
 }
 
+/*
+    Operator overload []
+    So user can use vector[index]
+*/
 template <class T>
 T &Vector<T>::operator[](int index) {
     if (index >= mSize)
@@ -114,6 +173,10 @@ T &Vector<T>::operator[](int index) {
     return data[index];
 }
 
+/*
+    Operator overload <<
+    so user can use vector with std::cout <<
+*/
 template <class T>
 std::ostream &operator<<(std::ostream out, const Vector<T> &v) {
     for (int i=0; i<v.size(); i++)
@@ -121,14 +184,20 @@ std::ostream &operator<<(std::ostream out, const Vector<T> &v) {
     return out;
 }
 
+/*
+    This method inserts the item at the index
+    supplied
+*/
 template<class T>
 void Vector<T>::insert(T item, u_int64_t index) {
+    // if greater than, resize array
     if (mLength++ >= mSize)
         resize();
-
+    
+    // shift everything over to the right from index supplied
     for (int i=mLength-1; i>=index; i--)
         data[i+1] = data[i];
-
+    // set item to index supplied
     data[index] = item;
 }
 
