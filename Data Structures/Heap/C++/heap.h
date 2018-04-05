905189f2a1081aa0394214aca6c03d980f13625e
Travis Avey <avey.travis@gmail.com>
2018-04-05 11:14:10 -0500

    Moved implementation out of class

--- Data Structures/Heap/C++/heap.h
+++ Data Structures/Heap/C++/heap.h
@@ -27,17 +27,145 @@ private:
     int count;              // current number of items in heap
     TYPE::type mType;       // the type, MAX or MIN
 
-    /*
-     * This method returns the parent index
+    int parent(int index) const;
+    int leftChild(int index) const;
+    int rightChild(int index) const;
+    void heapify();
+    void swap(int i, int j) ;
+
+
+public:
+
+    Heap();
+    explicit Heap(TYPE::type type);
+    bool isEmpty() const ;
+    int length() const;
+    void insert(T val);
+    T getMax() const;
+    T getMin() const;
+    void print();
+
+};
+
+/*
+     * Public constructor
      */
-    int parent(int index) const {
+    template<class T>
+    Heap::Heap() {
+        heap = new std::vector<T>;
+        count = 0;
+        mType = TYPE::MAX;
+
+    }
+
+    /*
+     * Constructor with TYPE
+     */
+    template<class T>
+    explicit Heap::Heap(TYPE::type type) {
+        heap = new std::vector<T>;
+        mType = type;
+        count = 0;
+    }
+
+    /*
+     * This method returns true if the heap is empty
+     */
+    template<class T>
+    bool Heap::isEmpty() const { return count == 0; }
+
+    /*
+     * This method returns the length of the heap
+     */
+    template<class T>
+    int Heap::length() const { return count; }
+
+    /*
+     * This method inserts a new value into the heap
+     */
+    template<class T>
+    void Heap::insert(T val) {
+        // add to heap vector
+        heap->push_back(val);
+        // call method to satisfy heap property
+        heapify();
+        // increment count
+        count++;
+    }
+
+    /*
+     * This method gets the max value from heap
+     */
+    template<class T>
+    T Heap::getMax() const {
+        // if empty, throw error
+        if (isEmpty())
+            throw std::out_of_range("Heap is empty");
+
+        // if we are a max type, max will be at root
+        if (mType == TYPE::MAX)
+            return heap->at(0);
+
+        // get the value at root
+        T val = heap->at(0);
+        // loop over rest of heap
+        for (int i=1; i<heap->size(); i++)
+            // if val is less than current
+            if (val < heap->at(i))
+                // set val current
+                val = heap->at(i);
+        // fall off loop, return max
+        return val;
+    }
+
+    /*
+     * This method returns the min of the heap
+     */
+    template<class T>
+    T Heap::getMin() const {
+        // if empty throw error
+        if (isEmpty())
+            throw std::out_of_range("Heap is empty");
+
+        // if min type, min at root
+        if (mType == TYPE::MIN)
+            return heap->at(0);
+
+        // get value at root
+        T val = heap->at(0);
+        // loop over rest of heap
+        for (int i=1; i<heap->size(); i++)
+            // if val is greater than current
+            if (val > heap->at(i))
+                // set val to current
+                val = heap->at(i);
+        // fall off loop, we have min
+        return val;
+    }
+
+    /*
+     * This method prints out the heap
+     */
+    template<class T>
+    void Heap::print() const {
+        for (auto val : *heap)
+            std::cout << val << ' ';
+        std::cout << std::endl;
+    }
+
+    /*
+    * This method returns the parent index
+    */
+    template<class T>
+    int Heap::parent(int index) const {
         return (index -1) / 2;
     }
 
     /*
      * This method returns the left child index
      */
-    int leftChild(int index) const {
+    template<class T>
+    int Heap::leftChild(int index) const {
         int left = 2 * index + 1;
         if (left >= count)
             return -1;
@@ -47,7 +175,8 @@ private:
     /*
      * This method returns the right child index
      */
-    int rightChild(int index) const {
+    template<class T>
+    int Heap::rightChild(int index) const {
         int right = 2 * index + 2;
         if (right >= count)
             return -1;
@@ -58,7 +187,8 @@ private:
      * This method maintains the heap property based
      * on MAX or MIN type
      */
-    void heapify() {
+    template<class T>
+    void Heap::heapify() {
         // get the count, starting at bottom of tree
         int index = count;
 
@@ -87,111 +217,10 @@ private:
      * Helper method to swap 2 values of the
      * heap, given the 2 indices location
      */
-    void swap(int i, int j) {
+    template<class T>
+    void Heap::swap(int i, int j) {
         std::swap(heap->at(i), heap->at(j));
     }
-
-
-public:
-    /*
-     * Public constructor
-     */
-    Heap() {
-        heap = new std::vector<T>;
-        count = 0;
-        mType = TYPE::MAX;
-
-    }
-
-    /*
-     * Constructor with TYPE
-     */
-    explicit Heap(TYPE::type type) {
-        heap = new std::vector<T>;
-        mType = type;
-        count = 0;
-    }
-
-    /*
-     * This method returns true if the heap is empty
-     */
-    bool isEmpty() const { return count == 0; }
-
-    /*
-     * This method returns the length of the heap
-     */
-    int length() const { return count; }
-
-    /*
-     * This method inserts a new value into the heap
-     */
-    void insert(T val) {
-        // add to heap vector
-        heap->push_back(val);
-        // call method to satisfy heap property
-        heapify();
-        // increment count
-        count++;
-    }
-
-    /*
-     * This method gets the max value from heap
-     */
-    T getMax() const {
-        // if empty, throw error
-        if (isEmpty())
-            throw std::out_of_range("Heap is empty");
-
-        // if we are a max type, max will be at root
-        if (mType == TYPE::MAX)
-            return heap->at(0);
-
-        // get the value at root
-        T val = heap->at(0);
-        // loop over rest of heap
-        for (int i=1; i<heap->size(); i++)
-            // if val is less than current
-            if (val < heap->at(i))
-                // set val current
-                val = heap->at(i);
-        // fall off loop, return max
-        return val;
-    }
-
-    /*
-     * This method returns the min of the heap
-     */
-    T getMin() const {
-        // if empty throw error
-        if (isEmpty())
-            throw std::out_of_range("Heap is empty");
-
-        // if min type, min at root
-        if (mType == TYPE::MIN)
-            return heap->at(0);
-
-        // get value at root
-        T val = heap->at(0);
-        // loop over rest of heap
-        for (int i=1; i<heap->size(); i++)
-            // if val is greater than current
-            if (val > heap->at(i))
-                // set val to current
-                val = heap->at(i);
-        // fall off loop, we have min
-        return val;
-    }
-
-    /*
-     * This method prints out the heap
-     */
-    void print() const {
-        for (auto val : *heap)
-            std::cout << val << ' ';
-        std::cout << std::endl;
-    }
-
-};
 
 
 #endif
