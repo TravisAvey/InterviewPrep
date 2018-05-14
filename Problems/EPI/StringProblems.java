d04f2b1d7e99e803b0fad4e41892ba54f7ca779d
Travis Avey <avey.travis@gmail.com>
2018-05-14 09:44:16 -0500

    Added Int to String method

--- Problems/EPI/StringProblems.java
+++ Problems/EPI/StringProblems.java
@@ -25,4 +25,47 @@ public class StringProblems {
         // fall off loop, it is a palindrome
         return true;
     }
+
+     /**
+     * This method converts the int 'x' to a string
+     *
+     * Time Complexity O(n)
+     *      where n is the number of digits of x
+     *
+     * @param x int
+     * @return string
+     */
+    public static String intToString(int x) {
+        // set a flag of is negative to false
+        boolean isNeg = false;
+
+        // if x is less than zero, is negative
+        if (x < 0)
+            isNeg = true;
+
+        // init a new string builder
+        StringBuilder sb = new StringBuilder();
+
+        // continue doing while x isn't zero
+        do {
+            // take the last digit convert to char
+            sb.append((char)('0' + Math.abs(x % 10)));
+            // remove that last digit
+            x /= 10;
+        } while (x != 0);
+
+        // if negative, append the negative sign
+        if (isNeg) {
+            sb.append('-');
+        }
+
+        // reverse the string
+        // because appending last digit in do..while loop
+        // is putting the digits in reverse order, so must
+        // reverse back to normal
+        sb.reverse();
+
+        // return the string
+        return sb.toString();
+    }
 }
