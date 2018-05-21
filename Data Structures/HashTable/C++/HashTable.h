035344e7e65dc53fe8e54e9382a3bfc09a0623ae
Travis Avey <avey.travis@gmail.com>
2018-05-21 10:35:07 -0500

    Working on hash table

--- Data Structures/HashTable/C++/HashTable.h
+++ Data Structures/HashTable/C++/HashTable.h
@@ -3,24 +3,20 @@
 
 #include <string>
 
-template <class K, class V>
-class Entry {
-private:
-    K mKey;
-    V mValue;
-
-public:
-
-    Entry *next;
-    Entry(K, V);
-    std::string toString() const;
-};
 
 template <class K, class V>
 class HashTable {
 private:
+
+    struct Entry<K, V> {
+        K key;
+        V value;
+        Entry *next;
+    };
+
+    Entry *next;
     const int SIZE = 16;
-    Entry<K, V> entries[];
+    Entry entries[];
 
     int getHash(K);
 public:
@@ -31,7 +27,7 @@ public:
 
 template <class K, class V>
 HashTable<K, V>::HashTable() {
-    entries = new Entry<K, V>[SIZE];
+    entries = new Entry[SIZE];
 
     for (int i=0; i<SIZE; i++)
         entries[i] = nullptr;
@@ -40,35 +36,32 @@ HashTable<K, V>::HashTable() {
 template <class K, class V>
 void HashTable<K, V>::put(K key, V value) {
     int hash = getHash(key);
-    Entry<K, V> *entry = new Entry<K, V>(key, value);
-    if (!entries[hash]) {
-        entries[hash] = entry;
-    } else {
-        Entry<K, V> temp = entries[hash];
+    auto *entry = new Entry;
+    entry->key = key;
+    entry->value = value;
+    entry->next = nullptr;
 
-        while (temp.next != null)
+    if (entries[hash].next == nullptr) {
+        entries[hash] = *entry;
+    } else {
+
+        Entry temp<V> = entry[hash];
+
+        while (temp.next != nullptr)
             temp = temp.next;
-        
+
         temp.next = entry;
     }
 }
 
 template <class K, class V>
 int HashTable<K, V>::getHash(K key) {
-    size_t hash = std::hash<K>{key};
-    return (int) hash % SIZE;
+    auto hash = std::hash<K>(key);
+
+    //int hash = std::hash<K>{key};
+    return  ((int)hash) % SIZE;
 }
 
-template <class K, class V>
-Entry<K, V>::Entry(K key, V value) {
-    mKey = key;
-    mValue = value;
-    next = nullptr;
-}
 
-template <class K, class V>
-std::string Entry<K, V>::toString() const {
-    return "[" + mKey + ":" + mValue + "]";
-}
 
 #endif
