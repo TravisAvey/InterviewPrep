0da6814b0201ae5daa950ef3f2d8fa5d699c94de
Travis Avey <avey.travis@gmail.com>
2018-05-31 10:58:11 -0500

    Changing up Linked List class
    Need to look at implementing the merge algorithm with new implementation.  May require moving out of class or accepting another linked list object and merging with current linked list object

--- Problems/EPI/LinkedList.java
+++ Problems/EPI/LinkedList.java
@@ -9,53 +9,119 @@
 public class LinkedList {
 
 
-    public static class ListNode {
-        int data;
-        ListNode next;
+    private Node head;
+
+    public LinkedList() {
+        head = null;
     }
 
-    public static ListNode searchList(ListNode node, int key) {
-        while (node != null && node.data != key) {
-            node = node.next;
-        }
-
-        return node;
+    public LinkedList(int data) {
+        head = new Node(data);
     }
 
-    public static void insertAfter(ListNode node, ListNode newNode) {
-        newNode.next = node.next;
-        node.next = newNode;
+    public int front() {
+        return head.getData();
     }
 
-    public static void deleteList(ListNode node) {
-        node.next = node.next.next;
+    public void pushFront(int data) {
+        Node current = new Node(data);
+        current.setNext(head);
+        head = current;
     }
 
-    public static void print(ListNode node) {
-        while (node != null) {
-            System.out.println(node.data);
-            node = node.next;
-        }
+    public int popFront() {
+        int data = head.getData();
+
+        head = head.getNext();
+
+        return data;
     }
 
-    public static ListNode mergeSortedLists(ListNode L1, ListNode L2) {
-        ListNode dummy = new ListNode();
-        ListNode current = dummy;
+    public void pushBack(int data) {
+        Node current = new Node(data);
 
-        while (L1 != null & L2 != null) {
-            if (L1.data <= L2.data) {
-                current.next = L1;
-                L1 = L1.next;
-            } else {
-                current.next = L2;
-                L2 = L2.next;
+        if (head == null) {
+            head = current;
+        } else {
+            Node ptr = head;
+            while (ptr.getNext() != null) {
+                ptr = ptr.getNext();
             }
 
-            current = current.next;
+            ptr.setNext(current);
         }
-
-        current.next = L1 != null ? L1 : L2;
-        return dummy.next;
     }
+
+    public void print() {
+        // if the head is null, print out empty list
+        if (this.head == null)
+            System.out.println("Empty List");
+        else
+        {
+            // print out the first
+            System.out.print(this.head.getData() + "->");
+            // create a node pointer at the next node
+            Node ptr = this.head.getNext();
+
+            // traverse the list; while the next is not null
+            while (ptr.getNext() != null)
+            {
+                // print out the current node's data
+                System.out.print(ptr.getData() + "->");
+                // move to the next node
+                ptr = ptr.getNext();
+            }
+            // print out the last node's data and a new line
+            System.out.print(ptr.getData() + "\n");
+        }
+    }
+
+    // TODO: recreate this method with new implementation... may need to do outside of class? or accept only one list and merge with current object
+//    public static ListNode mergeSortedLists(ListNode L1, ListNode L2) {
+//        ListNode dummy = new ListNode();
+//        ListNode current = dummy;
+//
+//        while (L1 != null & L2 != null) {
+//            if (L1.data <= L2.data) {
+//                current.next = L1;
+//                L1 = L1.next;
+//            } else {
+//                current.next = L2;
+//                L2 = L2.next;
+//            }
+//
+//            current = current.next;
+//        }
+//
+//        current.next = L1 != null ? L1 : L2;
+//        return dummy.next;
+//    }
 }
 
+class Node {
+    private int data;
+    private Node next;
+
+    public Node() {
+        next = null;
+    }
+
+    public Node(int data) {
+        this.data = data;
+        next = null;
+    }
+
+    void setNext(Node node) {
+        next = node;
+    }
+
+    Node getNext() {
+        return next;
+    }
+
+    int getData() {
+        return data;
+    }
+
+
+}
